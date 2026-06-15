703e77c1cc2dab6708286cce48986ef79f11dbae
Gabrieli Bergmann <gabrielibergmann1@gmail.com>
2026-06-15 20:36:45 -0300

    metodo de coluna cheia

--- src/Connect4.java
+++ src/Connect4.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Connect4 {
 
-    Scanner sc = new Scanner(System.in);
     public void tabuleiroBranco(char[][] tabuleiro) {
-        //adiciona ao tabuleiro "B" wm todas as linhas e colunas
+        // adiciona ao tabuleiro "B" wm todas as linhas e colunas
         for (int linha = 0; linha < 6; linha++) {
             for (int colunas = 0; colunas < 7; colunas++) {
                 tabuleiro[linha][colunas] = 'B';
@@ -16,16 +15,16 @@ public class Connect4 {
         for (int linha = 0; linha < 6; linha++) {
             for (int colunas = 0; colunas < 7; colunas++) {
                 System.out.print(tabuleiro[linha][colunas] + " ");
-                
+
             }
 
             System.out.println();
         }
         System.out.println("1 2 3 4 5 6 7");
-        //contagem de colunas
+        // contagem de colunas
     }
 
-    public void escolherCor(){
+    public char[] escolherCor(Scanner sc) {
         System.out.println("Escolher cor!");
         System.out.println("1 - Vermelho (v): ");
         System.out.println("2 - Azul (a): ");
@@ -33,7 +32,7 @@ public class Connect4 {
 
         char corJogador;
         char corComputador;
-        if(cor == 'v'){
+        if (cor == 'v') {
             corJogador = 'v';
             corComputador = 'a';
             System.out.println("Voce escolheu vermelho. Computador é azul");
@@ -42,32 +41,50 @@ public class Connect4 {
             corComputador = 'v';
             System.out.println("Voce escolheu azul. Computador é vermelho");
         }
+        return new char[] { corJogador, corComputador };
     }
 
-    public void adicionarPeca(char cor, char[][] tabuleiro, Scanner sc){
+    public void adicionarPeca(char cor, char[][] tabuleiro, Scanner sc) {
         int coluna;
         ImprimirTabuleiro(tabuleiro);
-        System.out.println("Digite a coluna que deseja acrescentar a peça: (1 á 7)");
-        coluna = sc.nextInt() -1;
+        do {
+            System.out.println("Digite a coluna que deseja acrescentar a peça: (1 á 7)");
+            coluna = sc.nextInt() - 1;
 
-        //roda as linhas de baixo pra cima colocando sempre na mais baixa disponivel
-        for (int linha = 5; linha >=0; linha--){
-           if (tabuleiro[linha][coluna] == 'B'){
-            tabuleiro[linha][coluna] = cor;
-            break;
-           }
+            if (colunaCheia(tabuleiro, coluna)) {
+                System.out.println("Coluna cheia! Escolha outra");
+            }
+
+        } while (colunaCheia(tabuleiro, coluna));// repete enquanto a coluna estiver cheia
+
+        // roda as linhas de baixo pra cima colocando sempre na mais baixa disponivel
+        for (int linha = 5; linha >= 0; linha--) {
+            if (tabuleiro[linha][coluna] == 'B') {
+                tabuleiro[linha][coluna] = cor;
+                break;
+            }
         }
         ImprimirTabuleiro(tabuleiro);
     }
 
-    public Connect4() {
-        //todos os atributos devem ser no construtor
-        Scanner sc = new Scanner(System.in);
-       char tabuleiro[][] = new char[6][7];
+    public boolean colunaCheia(char[][] tabuleiro, int coluna) {
+        // se a ultima linha da coluna não for b entao está cheia
+        if (tabuleiro[0][coluna] != 'B') {
+            return true;
+        }
+        return false;
+    }
 
-        tabuleiroBranco(tabuleiro); 
-        escolherCor();
-        adicionarPeca('v', tabuleiro, sc);
+    public Connect4() {
+        // todos os atributos devem ser no construtor
+        Scanner sc = new Scanner(System.in);
+        char tabuleiro[][] = new char[6][7];
+        tabuleiroBranco(tabuleiro);
+        char[] cores = escolherCor(sc);
+        char corJogador = cores[0]; // cor que o jogador escolheu
+        char corComputador = cores[1]; // cor do computador
+
+        adicionarPeca(corJogador, tabuleiro, sc);
 
         sc.close();
     }
